98fc22ddec736641c4cb74efc72904f22df0409a
Yuhua Qi <fatmoonqyp@126.com>
2019-03-29 07:50:56 +0800

    Third change. Add sqaure.cpp for test.

---
+++ src/square.cpp
@@ -0,0 +1,222 @@
+/***************************************************************************************************************************
+ * square.cpp
+ *
+ * Author: Qyp
+ *
+ * Update Time: 2018.8.17
+ *
+ * 说明: mavros正方形飞行示例程序
+ *      1.
+ *      2.
+ *      3.
+***************************************************************************************************************************/
+
+#include <ros/ros.h>
+#include <fstream>
+#include <math.h>
+#include <string>
+#include <time.h>
+#include <queue>
+#include <vector>
+#include <cstdlib>
+#include <stdlib.h>
+#include <iostream>
+#include <stdio.h>
+#include <std_msgs/Bool.h>
+#include <px4_command/command.h>
+
+
+using namespace std;
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>全 局 变 量<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+enum Command
+{
+    Move_ENU,
+    Move_Body,
+    Hold,
+    Land,
+    Disarm,
+    Failsafe_land,
+    Idle
+};
+px4_command::command Command_now;
+//---------------------------------------正方形参数---------------------------------------------
+float size_square;                  //正方形边长
+float height_square;                //飞行高度
+float sleep_time;
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "square");
+    ros::NodeHandle nh("~");
+
+    // 频率 [1hz]
+    ros::Rate rate(1.0);
+
+    // 【发布】发送给position_control.cpp的命令
+    ros::Publisher move_pub = nh.advertise<px4_command::command>("/px4/Command", 10);
+
+    //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>参数读取<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    nh.param<float>("size_square", size_square, 1.5);
+    nh.param<float>("height_square", height_square, 1.0);
+    nh.param<float>("sleep_time", sleep_time, 10.0);
+
+
+
+    int check_flag;
+    // 这一步是为了程序运行前检查一下参数是否正确
+    // 输入1,继续，其他，退出程序
+    cout << "size_square: "<<size_square<<"[m]"<<endl;
+    cout << "height_square: "<<height_square<<"[m]"<<endl;
+    cout << "Please check the parameter and setting，1 for go on， else for quit: "<<endl;
+    cin >> check_flag;
+
+    if(check_flag != 1)
+    {
+        return -1;
+    }
+
+
+    int i = 0;
+    int comid = 0;
+
+    //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>主程序<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    //依次发送4个目标点给position_control.cpp
+    //第一个目标点，左下角
+    i = 0;
+    while (i < sleep_time)
+    {
+
+        Command_now.command = Move_ENU;  //Move模式
+        Command_now.sub_mode = 0;             //子模式：位置控制模式
+        Command_now.pos_sp[0] = -size_square/2;
+        Command_now.pos_sp[1] = -size_square/2;
+        Command_now.pos_sp[2] = height_square;
+        Command_now.yaw_sp = 0;
+        Command_now.comid = comid;
+        comid++;
+
+        move_pub.publish(Command_now);
+
+        rate.sleep();
+
+        cout << "Point 1"<<endl;
+
+        i++;
+
+    }
+
+
+
+    //第二个目标点，左上角
+    i = 0;
+    while (i < sleep_time)
+    {
+
+        Command_now.command = Move_ENU;  //Move模式
+        Command_now.sub_mode = 0;             //子模式：位置控制模式
+        Command_now.pos_sp[0] = size_square/2;
+        Command_now.pos_sp[1] = -size_square/2;
+        Command_now.pos_sp[2] = height_square;
+        Command_now.yaw_sp = 0;
+        Command_now.comid = comid;
+        comid++;
+
+        move_pub.publish(Command_now);
+
+        rate.sleep();
+
+        cout << "Point 2"<<endl;
+
+        i++;
+
+    }
+
+    //第三个目标点，右上角
+    i = 0;
+    while (i < sleep_time)
+    {
+
+        Command_now.command = Move_ENU;  //Move模式
+        Command_now.sub_mode = 0;             //子模式：位置控制模式
+        Command_now.pos_sp[0] = size_square/2;
+        Command_now.pos_sp[1] = size_square/2;
+        Command_now.pos_sp[2] = height_square;
+        Command_now.yaw_sp = 0;
+        Command_now.comid = comid;
+        comid++;
+
+        move_pub.publish(Command_now);
+
+        rate.sleep();
+
+        cout << "Point 3"<<endl;
+
+        i++;
+
+    }
+
+    //第四个目标点，右下角
+    i = 0;
+    while (i < sleep_time)
+    {
+
+        Command_now.command = Move_ENU;  //Move模式
+        Command_now.sub_mode = 0;             //子模式：位置控制模式
+        Command_now.pos_sp[0] = -size_square/2;
+        Command_now.pos_sp[1] = size_square/2;
+        Command_now.pos_sp[2] = height_square;
+        Command_now.yaw_sp = 0;
+        Command_now.comid = comid;
+        comid++;
+
+        move_pub.publish(Command_now);
+
+        rate.sleep();
+
+        cout << "Point 4"<<endl;
+
+        i++;
+
+    }
+
+    //第五个目标点，回到起点
+    i = 0;
+    while (i < sleep_time)
+    {
+
+        Command_now.command = Move_ENU;  //Move模式
+        Command_now.sub_mode = 0;             //子模式：位置控制模式
+        Command_now.pos_sp[0] = -size_square/2;
+        Command_now.pos_sp[1] = -size_square/2;
+        Command_now.pos_sp[2] = height_square;
+        Command_now.yaw_sp = 0;
+        Command_now.comid = comid;
+        comid++;
+
+        move_pub.publish(Command_now);
+
+        rate.sleep();
+
+        cout << "Point 5"<<endl;
+
+        i++;
+
+    }
+
+    //降落
+
+
+
+    Command_now.command = Land;
+    move_pub.publish(Command_now);
+
+    rate.sleep();
+
+    cout << "Land"<<endl;
+
+
+
+
+
+    return 0;
+}
