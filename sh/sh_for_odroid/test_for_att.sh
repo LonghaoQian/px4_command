196b29b56c7e45b5bb51c20cfcc66c9b8871340d
FSC lab Odroid <fatmoonqyp@126.com>
2019-04-05 20:15:36 +0000

    Fix small bugs

--- sh/sh_for_odroid/test_for_att.sh
+++ sh/sh_for_odroid/test_for_att.sh
@@ -1,7 +1,7 @@
 ##
 mate-terminal --window -e 'bash -c "roscore; exec bash"' \
---tab -e 'bash -c "sleep 2; rosrun px4_command fake_vicon; exec bash"' \
---tab -e 'bash -c "sleep 2; roslaunch mavros px4.launch fcu_url:="/dev/ttyTHS2:921600"; exec bash"' \
+--tab -e 'bash -c "sleep 3; roslaunch vrpn_client_ros sample.launch server:=192.168.1.128; exec bash"' \
+--tab -e 'bash -c "sleep 2; roslaunch mavros px4.launch fcu_url:="/dev/ttyUSB0:921600"; exec bash"' \
 --tab -e 'bash -c "sleep 2; roslaunch px4_command px4_pos_estimator.launch; exec bash"' \
 --tab -e 'bash -c "sleep 2; roslaunch px4_command px4_pos_att_controller.launch; exec bash"' \
 --tab -e 'bash -c "sleep 2; rosrun px4_command move; exec bash"' \
