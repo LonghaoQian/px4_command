b9a17b10dd5d6003a59575d229a54ae4d49e735f
Qi <fatmoonqyp@126.com>
2019-06-09 02:35:41 +0800

    Add a new application: payload_drop
    Signed-off-by: Qi <fatmoonqyp@126.com>

--- sh/sh_for_odroid/test_for_passivity.sh
+++ sh/sh_for_odroid/test_for_passivity.sh
@@ -1,6 +1,6 @@
 ##
 mate-terminal --window -e 'bash -c "roscore; exec bash"' \
---tab -e 'bash -c "sleep 3; roslaunch vrpn_client_ros sample.launch server:=192.168.1.128; exec bash"' \
+--tab -e 'bash -c "sleep 3; roslaunch vrpn_client_ros sample.launch server:=192.168.1.62; exec bash"' \
 --tab -e 'bash -c "sleep 3; roslaunch mavros px4.launch fcu_url:="/dev/ttyUSB0:921600"; exec bash"' \
 --tab -e 'bash -c "sleep 4; roslaunch px4_command px4_pos_estimator.launch; exec bash"' \
 --tab -e 'bash -c "sleep 4; roslaunch px4_command px4_pos_controller_passivity.launch; exec bash"' \
