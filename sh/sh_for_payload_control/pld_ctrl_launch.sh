b624a2213c151b28a621b57b16751e11d0f2a794
longhao <longhao.qian@mail.utoronto.ca>
2019-10-19 16:27:14 -0400

    add general purpose launch script

---
+++ sh/sh_for_payload_control/pld_ctrl_launch.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+xterm -e "roslaunch px4_command mavros_multi_drone.launch uavID:=uav0" &
+xterm -e "roslaunch px4_command px4_multidrone_pos_estimator_pure_vision.launch uavID:=uav0" &
+xterm -e "roslaunch px4_command px4_multidrone_pos_controller_gazebo.launch uavID:=uav0"
