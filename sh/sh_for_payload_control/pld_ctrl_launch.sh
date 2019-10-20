bd818d754831f926ea7c9614bd01ddc0841302b7
longhao <longhao.qian@mail.utoronto.ca>
2019-10-20 15:03:35 -0400

    fixed a bug in sh script

--- sh/sh_for_payload_control/pld_ctrl_launch.sh
+++ sh/sh_for_payload_control/pld_ctrl_launch.sh
@@ -2,4 +2,4 @@
 
 xterm -e "roslaunch px4_command mavros_multi_drone.launch uavID:=uav0" &
 xterm -e "roslaunch px4_command px4_multidrone_pos_estimator_pure_vision.launch uavID:=uav0" &
-xterm -e "roslaunch px4_command px4_multidrone_pos_controller_gazebo.launch uavID:=uav0"
+xterm -e "roslaunch px4_command px4_multidrone_pos_controller.launch uavID:=uav0"
